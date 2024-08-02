e64d64e39dd39571853dae771ca5798da78c4b1b
Maximilian Schulten <maximilian.schulten1@gmail.com>
2024-08-02 11:26:21 -0400

    Making the sliders change orientation

--- src/DemoViewer.java
+++ src/DemoViewer.java
@@ -42,19 +42,45 @@ public class DemoViewer {
         // Panel that displays render results
         JPanel renderPanel = new JPanel() {
           public void paintComponent(Graphics g) {
+              // Initializing background
               Graphics2D g2 = (Graphics2D) g;
               g2.setColor(Color.BLACK);
               g2.fillRect(0, 0, getWidth(), getHeight());
 
-              //Drawing the triangle
+              // Adding the transform functionality so sliders change view points
+              double heading = Math.toRadians(headingSlider.getValue());
+
+              Matrix3 headingTransform = new Matrix3(
+                      new double[] {
+                              Math.cos(heading), 0, -Math.sin(heading),
+                              0, 1, 0,
+                              Math.sin(heading), 0, Math.cos(heading)
+                      }
+              );
+
+              double pitch = Math.toRadians(pitchSlider.getValue());
+
+              Matrix3 pitchTransform = new Matrix3(new double[]{
+                      1, 0, 0,
+                      0, Math.cos(pitch), Math.sin(pitch),
+                      0, -Math.sin(pitch), Math.cos(pitch)
+              }
+              );
+
+              Matrix3 transform = headingTransform.multiply(pitchTransform);
+
               g2.translate(getWidth()/2, getHeight()/2);
               g2.setColor(Color.WHITE);
-              // Drawing all the triangles in tris
-              for(Triangle t : tris) {
+
+              // Transform every vertex for each triangle
+              for (Triangle t : tris) {
+                  Vertex v1 = transform.transform(t.v1);
+                  Vertex v2 = transform.transform(t.v2);
+                  Vertex v3 = transform.transform(t.v3);
                   Path2D path = new Path2D.Double();
-                  path.moveTo(t.v1.x, t.v1.y);
-                  path.lineTo(t.v2.x, t.v2.y);
-                  path.lineTo(t.v3.x, t.v3.y);
+                  path.moveTo(v1.x, v1.y);
+                  path.lineTo(v2.x, v2.y);
+                  path.lineTo(v3.x, v3.y);
                   path.closePath();
                   g2.draw(path);
               }
@@ -62,6 +88,9 @@ public class DemoViewer {
         };
 
         pane.add(renderPanel, BorderLayout.CENTER);
+
+        headingSlider.addChangeListener(e -> renderPanel.repaint());
+        pitchSlider.addChangeListener(e-> renderPanel.repaint());
 
         frame.setSize(400, 400);
         frame.setVisible(true);
@@ -90,5 +119,32 @@ class Triangle {
         this.v2 = v2;
         this.v3 = v3;
         this.color = color;
+    }
+}
+
+class  Matrix3 {
+    double[] values;
+    Matrix3(double[] values) {
+        this.values = values;
+    }
+    Matrix3 multiply(Matrix3 other) {
+        double[] result = new double[9];
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                for (int i = 0; i < 3; i++) {
+                    result[row * 3 + col] +=
+                            this.values[row * 3 + i] * other.values[i * 3 + col];
+                }
+            }
+        }
+        return new Matrix3(result);
+    }
+
+    Vertex transform(Vertex in) {
+        return new Vertex(
+                in.x * values[0] + in.y * values[3] + in.z * values[6],
+                in.x * values[1] + in.y * values[4] + in.z * values[7],
+                in.x * values[2] + in.y * values[5] + in.z * values[8]
+        );
     }
 }
