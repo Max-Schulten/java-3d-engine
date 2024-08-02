e1aa4b7bd24b2dca525794c74a697d419905c392
Maximilian Schulten <maximilian.schulten1@gmail.com>
2024-08-02 12:13:13 -0400

    added shading

--- src/DemoViewer.java
+++ src/DemoViewer.java
@@ -1,9 +1,25 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Path2D;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class DemoViewer {
+
+
+    // Gets shading of colors
+    public static Color getShade(Color color, double shade) {
+        double redLinear = Math.pow(color.getRed(), 2.4) * shade;
+        double greenLinear = Math.pow(color.getGreen(), 2.4) * shade;
+        double blueLinear = Math.pow(color.getBlue(), 2.4) * shade;
+
+        int red = (int) Math.pow(redLinear, 1/2.4);
+        int green = (int) Math.pow(greenLinear, 1/2.4);
+        int blue = (int) Math.pow(blueLinear, 1/2.4);
+
+        return new Color(red, green, blue);
+    }
 
     public static void main(String[] args) {
         // Drawing a triangle
@@ -69,21 +85,81 @@ public class DemoViewer {
 
               Matrix3 transform = headingTransform.multiply(pitchTransform);
 
-              g2.translate(getWidth()/2, getHeight()/2);
-              g2.setColor(Color.WHITE);
+              // Use barycentric coordinates to draw the pixels in the triangle with color
+              BufferedImage img = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
 
-              // Transform every vertex for each triangle
+              // Initializing zbuffer
+              double[] zBuffer = new double[img.getWidth() * img.getHeight()];
+              Arrays.fill(zBuffer, Double.NEGATIVE_INFINITY);
+
+              // Rendering each triangle
+
               for (Triangle t : tris) {
                   Vertex v1 = transform.transform(t.v1);
                   Vertex v2 = transform.transform(t.v2);
                   Vertex v3 = transform.transform(t.v3);
-                  Path2D path = new Path2D.Double();
-                  path.moveTo(v1.x, v1.y);
-                  path.lineTo(v2.x, v2.y);
-                  path.lineTo(v3.x, v3.y);
-                  path.closePath();
-                  g2.draw(path);
+
+                  // Perform manual translation
+                  v1.x += getWidth() / 2;
+                  v1.y += getHeight() / 2;
+                  v2.x += getWidth() / 2;
+                  v2.y += getHeight() / 2;
+                  v3.x += getWidth() / 2;
+                  v3.y += getHeight() / 2;
+
+                  Vertex ab = new Vertex(v2.x - v1.x, v2.y - v1.y, v2.z - v1.z);
+                  Vertex ac = new Vertex(v3.x - v1.x, v3.y - v1.y, v3.z - v1.z);
+
+                  Vertex norm = new Vertex(
+                          ab.y * ac.z - ab.z * ac.y,
+                          ab.z * ac.x - ab.x * ac.z,
+                          ab.x * ac.y - ab.y * ac.x
+                  );
+
+                  double normalLength =
+                          Math.sqrt(norm.x * norm.x + norm.y * norm.y + norm.z * norm.z);
+                  norm.x /= normalLength;
+                  norm.y /= normalLength;
+                  norm.z /= normalLength;
+
+                  double angleCos = Math.abs(norm.z);
+
+                  // Find bounds
+
+                  int minX = (int) Math.max(0, Math.ceil(Math.min(v1.x, Math.min(v2.x, v3.x))));
+                  int maxX = (int) Math.min(img.getWidth() - 1,
+                          Math.floor(Math.max(v1.x, Math.max(v2.x, v3.x))));
+                  int minY = (int) Math.max(0, Math.ceil(Math.min(v1.y, Math.min(v2.y, v3.y))));
+                  int maxY = (int) Math.min(img.getHeight() - 1,
+                          Math.floor(Math.max(v1.y, Math.max(v2.y, v3.y))));
+
+                  double triangleArea = (v1.y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - v1.x);
+
+                  // Iterate over all the points, if they are in the triangle draw them
+                  for (int y = minY; y <= maxY; y++) {
+                      for (int x = minX; x <= maxX; x++) {
+                          double b1 =
+                                  ((y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - x)) / triangleArea;
+                          double b2 =
+                                  ((y - v1.y) * (v3.x - v1.x) + (v3.y - v1.y) * (v1.x - x)) / triangleArea;
+                          double b3 =
+                                  ((y - v2.y) * (v1.x - v2.x) + (v1.y - v2.y) * (v2.x - x)) / triangleArea;
+
+
+                          if (b1 >= 0 && b1 <= 1 && b2 >= 0 && b2 <= 1 && b3 >= 0 && b3 <= 1) {
+                              double depth = b1 * v1.z + b2 * v2.z + b3 * v3.z;
+                              int zIndex = y * img.getWidth() + x;
+                              if (zBuffer[zIndex] < depth) {
+                                  img.setRGB(x, y, getShade(t.color, angleCos).getRGB());
+                                  zBuffer[zIndex] = depth;
+                              }
+                          }
+
+                      }
+                  }
               }
+              g2.drawImage(img, 0, 0, null);
+
           }
         };
 
